36a9363bcbbe78c1f2e6e14dd36cc5c3b0e6f6cf
rannaraabe <rannaraabe@hotmail.com>
2020-03-29 17:30:29 -0300

    removendo grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(AppNotes());
+void main() {
+  runApp(AppNotes());
+}
 
 class AppNotes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notes',
+      title: 'Notepad',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: HomePage(title: 'Notes'),
+      home: HomePage(title: 'Notepad'),
     );
   }
 }
@@ -21,12 +23,14 @@ class HomePage extends StatefulWidget {
   final String title;
 
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   void addNote() {
     setState(() {
+      // create a note
+      // adicionar um container
     });
   }
 
@@ -36,35 +40,12 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: CustomScrollView(
-        primary: false,
-        slivers: <Widget>[
-          SliverPadding(
-            padding: const EdgeInsets.all(20),
-            sliver: SliverGrid.count(
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 2,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Músicas para o jantar romântico:'),
-                  color: Colors.yellow[100],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Músicas para lavar o banheiro:'),
-                  color: Colors.yellow[200],
-                ),
-
-              ],
-            ),
-          ),
-        ],
+      body: Center(
+        child: Text('Press the button for add a note!')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: addNote,
-        tooltip: 'Adicione note',
+        tooltip: 'Add',
         child: Icon(Icons.add),
       ), 
     );
